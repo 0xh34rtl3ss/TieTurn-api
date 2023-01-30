7937021ad47a25151d19c5d4672329d26eebde6b
me-ran <75988843+0xh34rtl3ss@users.noreply.github.com>
2023-01-31 00:56:24 +0800

    update

--- app.py
+++ app.py
@@ -3,11 +3,10 @@ from flask import Flask, request
 app = Flask(__name__)
 
 @app.route('/completed', methods=['POST'])
-def process_post():
-    data = request.get_json()
-    # process the data received in the POST request
-    # ...
-    return "Data processed successfully", 200
+def complete():
+    name = request.form.get('name')
+    print(name)
+    return '', 200
 
 if __name__ == '__main__':
     app.run()
