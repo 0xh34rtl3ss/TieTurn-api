daa98ca1e84fdf5dc3ef64b439bb1827f8ef0e78
me-ran <75988843+0xh34rtl3ss@users.noreply.github.com>
2023-01-31 01:09:14 +0800

    update datas

--- app.py
+++ app.py
@@ -5,7 +5,15 @@ app = Flask(__name__)
 @app.route('/completed', methods=['POST'])
 def complete():
     name = request.form.get('name')
+    email = request.form.get('email')
+    phone = request.form.get('phone')
+    branch = request.form.get('branch')
+    datetime = request.form.get('datetime')
     print(name)
+    print(email)
+    print(phone)
+    print(branch)
+    print(datetime)
     return '', 200
 
 if __name__ == '__main__':
