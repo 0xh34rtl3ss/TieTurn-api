d0ba225852a05467954ac0c94644f10a3b6350e3
me-ran <75988843+0xh34rtl3ss@users.noreply.github.com>
2023-01-31 12:11:14 +0800

    test

--- app.py
+++ app.py
@@ -5,69 +5,74 @@ import ssl
 import smtplib
 import random
 
-app = Flask(__name__)
+# app = Flask(__name__)
 
-@app.route('/completed', methods=['POST'])
-def complete():
-    name = request.form.get('name')
-    email = request.form.get('email')
-    phone = request.form.get('phone')
-    branch = request.form.get('branch')
-    datetime = request.form.get('datetime')
+# @app.route('/completed', methods=['POST'])
+# def complete():
+    # name = request.form.get('name')
+    # email = request.form.get('email')
+    # phone = request.form.get('phone')
+    # branch = request.form.get('branch')
+    # datetime = request.form.get('datetime')
+name = 'imran'
+email = 'imranmohamad26@gmail.com'
+phone = '01111921530'
+branch = 'bahau'
+datetime = '00000313'
     
-    sender = 'ychqdcexqcqt26@gmail.com'
-    emailPass = environ.get('email_pass')
-    receiver = email
+sender = 'ychqdcexqcqt26@gmail.com'
+emailPass = environ.get('email_pass')
+receiver = email
     
-    subject = 'RHB Online Appointment'
-    body = """
-    Hello %s,
-    Thank you for using our RHB Online Appointment service.
+subject = 'RHB Online Appointment'
+body = """
+Hello %s,
+Thank you for using our RHB Online Appointment service.
     
-    Here are the details of your appointment details:
+Here are the details of your appointment details:
     
-    Queue Number : %d
+Queue Number : %d
     
-    Name : %s
-    Phone Number : %s
-    Preferred branch : %s
-    Date : %s
-    Time : %s
+Name : %s
+Phone Number : %s
+Preferred branch : %s
+Date : %s
+Time : %s
     
-    we advise you to be in the branch 10 minutes early before your schedule time.
-    Thank you for choosing RHB Bank.
-    
-    
+we advise you to be in the branch 10 minutes early before your schedule time.
+Thank you for choosing RHB Bank.
     
     
     
     
     
     
-    This is an automatic generated message, do not reply to this email.
     
-    """ % (name, random.randint(111,999) , name, phone,branch,datetime,datetime )
     
-    em = EmailMessage()
-    em['from'] = sender
-    em['to'] = receiver
-    em['subject'] = subject
-    em.set_content(body)
+This is an automatic generated message, do not reply to this email.
     
-    context = ssl.create_default_context()
-    with smtplib.SMTP_SSL('smtp.gmail.com',465,context=context) as smtp:
-        smtp.login(sender,emailPass)
-        smtp.sendmail(sender,receiver,em.as_string())    
+""" % (name, random.randint(111,999) , name, phone,branch,datetime,datetime )
+    
+em = EmailMessage()
+em['from'] = sender
+em['to'] = receiver
+em['subject'] = subject
+em.set_content(body)
+    
+context = ssl.create_default_context()
+with smtplib.SMTP_SSL('smtp.gmail.com',465,context=context) as smtp:
+    smtp.login(sender,emailPass)
+    smtp.sendmail(sender,receiver,em.as_string())    
     
     
 
-    print(name)
-    print(email)
-    print(phone)
-    print(branch)
-    print(datetime)
+print(name)
+print(email)
+print(phone)
+print(branch)
+print(datetime)
     #dah dpt masuk data dkt api flask , tinggl nak send email je.
-    return '', 200
+#     return '', 200
 
-if __name__ == '__main__':
-    app.run()
+# if __name__ == '__main__':
+#     app.run()
